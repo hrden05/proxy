b86147f64a19e7bd4b603559f5e878b289039ada
Joshua T Miltier <32140184+JMiltier@users.noreply.github.com>
2020-07-03 20:07:42 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use('/api/images/', proxy(config.proxies.images, {
 // product info (topSideBar) // !DONE
 // 7777
 app.use('/product-info', proxy(config.proxies.productinfo));
-app.use('/api/stores', proxy(config.proxies.productinfo, {
+app.use('/', proxy(config.proxies.productinfo, {
   ProxyReqPathResolver: (req) => {
     const parts = req.url.split('?');
     const queryString = parts[1];
@@ -34,7 +34,7 @@ app.use('/api/stores', proxy(config.proxies.productinfo, {
   },
 }));
 
-app.use('/api/products', proxy(config.proxies.productinfo, {
+app.use('/', proxy(config.proxies.productinfo, {
   ProxyReqPathResolver: (req) => {
     const parts = req.url.split('?');
     const queryString = parts[1];
@@ -45,7 +45,7 @@ app.use('/api/products', proxy(config.proxies.productinfo, {
 // reviews // !DONE
 // 6969
 app.use('/reviews', proxy(config.proxies.reviews));
-app.use('/', proxy(config.proxies.reviews));
+app.use('/api/reviews', proxy(config.proxies.reviews));
 
 // product description  // !DONE
 // 4507
