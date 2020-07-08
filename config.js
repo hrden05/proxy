acbe61a1a22a29eb9e6a1b85aed36f5d59c312b8
Joshua T Miltier <32140184+JMiltier@users.noreply.github.com>
2020-07-08 11:08:57 -0600

    Updated AWS address for productinfo

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ const dev = {
   },
   proxies: {
     images: 'http://ec2-18-218-73-45.us-east-2.compute.amazonaws.com/',
-    productinfo: 'http://ec2-18-216-167-250.us-east-2.compute.amazonaws.com/',
+    productinfo: 'http://ec2-3-15-227-194.us-east-2.compute.amazonaws.com/',
     reviews: 'http://ec2-3-21-205-61.us-east-2.compute.amazonaws.com/',
     // productdescription: 'http://localhost:4507',
-    productdescription: 'http://ec2-18-219-201-12.us-east-2.compute.amazonaws.com',
+    productdescription: 'http://ec2-18-219-201-12.us-east-2.compute.amazonaws.com/',
     similar: 'http://ec2-18-222-11-65.us-east-2.compute.amazonaws.com/',
     // similar: 'http://localhost:3002',
   },
