e0586d697d1f4bf2dc80b5c3db1f43189da02950
Joshua T Miltier <32140184+JMiltier@users.noreply.github.com>
2020-08-01 11:34:43 -0600

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const dev = {
   proxies: {
     images: 'http://ec2-18-218-73-45.us-east-2.compute.amazonaws.com/',
     productinfo: 'http://ec2-3-15-227-194.us-east-2.compute.amazonaws.com/',
-    reviews: 'http://ec2-3-21-205-61.us-east-2.compute.amazonaws.com/',
+    reviews: 'http://ec2-18-218-223-162.us-east-2.compute.amazonaws.com/',
     // productdescription: 'http://localhost:4507',
     productdescription: 'http://ec2-18-219-110-43.us-east-2.compute.amazonaws.com',
     similar: 'http://ec2-18-221-89-163.us-east-2.compute.amazonaws.com/',
